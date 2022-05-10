5f16e4618fedb044501dcd2e32674148a0ae353d
yuanliwei <yuanliwei@daojia-inc.com>
2022-05-10 13:06:12 +0800

    三种方法

--- java/src/array/ContainsDuplicate.java
+++ java/src/array/ContainsDuplicate.java
@@ -1,5 +1,9 @@
 package array;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every
  * element is distinct.
@@ -20,6 +24,37 @@ public class ContainsDuplicate {
                 if (nums[i] == nums[j]) {
                     return Boolean.TRUE;
                 }
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    /**
+     * 排序后，相邻数值对比
+     *
+     * @param nums
+     * @return
+     */
+    boolean solution2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    /**
+     * 利用哈市的不可重复性，来处理，
+     * @param nums
+     * @return
+     */
+    boolean solution3(int[] nums) {
+        Set<Integer> hash = new HashSet<>();
+        for (int i : nums) {
+            if (!hash.add(i)) {
+                return Boolean.TRUE;
             }
         }
         return Boolean.FALSE;
